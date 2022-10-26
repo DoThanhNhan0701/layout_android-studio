b867e19694d23da3e5a65affa5f2ecff63fce779
DoThanhNhan0701 <TNhan.ITTechnology@gmail.com>
2022-10-26 22:34:41 +0700

    Update bai

---
+++ app/src/main/java/com/example/framelayout/EvenCLick1Activity.java
@@ -0,0 +1,14 @@
+package com.example.framelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EvenCLick1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_even_click1);
+    }
+}
